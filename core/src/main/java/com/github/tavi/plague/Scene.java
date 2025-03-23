8487e2b803ad64fcc949c18172675afba7585d6d
Moonlight <moonlight9512@gmail.com>
2025-03-23 21:37:45 +0500

    Add asset manager billet

--- core/src/main/java/com/github/tavi/plague/Scene.java
+++ core/src/main/java/com/github/tavi/plague/Scene.java
@@ -1,9 +1,13 @@
 package com.github.tavi.plague;
 
 import com.badlogic.gdx.Screen;
+import com.github.tavi.plague.shared.Assets;
 
 /** The main implementation of LibGDX's Screen, used in Plague Engine. */
 public class Scene implements Screen {
+	
+	private Assets assets = Assets.get();
+	
     @Override
     public void show() {
         // Prepare your screen here.
@@ -11,7 +15,16 @@ public class Scene implements Screen {
 
     @Override
     public void render(float delta) {
-        // Draw your screen here. "delta" is the time since last render in seconds.
+    	if (!assets.update()) {
+    		System.out.print((int) (assets.getProgress() * 100));
+    		System.out.println("%");
+    	}
+        
+        assets.batch().begin();
+        
+        assets.batch().draw(assets.texture("textures/real-male/male_torso_0.png"), 20, 20);
+        
+        assets.batch().end();
     }
 
     @Override
